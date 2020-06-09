412dd9246875e3aca5f8ec0481e6440a8d1b99e9
Thiago <thiagohcarvalho81@hotmail.com>
2020-06-09 00:59:17 -0300

    Attributes, getters, setters and methods added to the Product class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import entities.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Product prod = new Product("TV", 200.0);
 		
+		System.out.println(prod.priceTag());
 		
 	}
 
